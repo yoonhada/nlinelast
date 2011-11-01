f67d9608c8e39f23c04940ccdf5e5cfc614d75b3
chunju2@naver.com <chunju2@naver.com@d02aaf57-2019-c8cd-d06c-d029ef2af4e0>
2011-11-01 05:17:19 +0000

    AI 모듈 추가

---
+++ LastProject/Source/Melee.cpp
@@ -0,0 +1,29 @@
+#include "stdafx.h"
+
+#include "Melee.h"
+
+
+Melee& Melee::GetInstance()
+{
+	static Melee Instance;
+
+	return &Instance;
+}
+
+
+VOID Melee::Enter( Miner* pMiner )
+{
+
+}
+
+
+VOID Melee::Execute( Miner* pMiner )
+{
+
+}
+
+
+VOID Melee::Exit( Miner* pMiner )
+{
+
+}
---
+++ LastProject/Source/Spin.cpp
@@ -0,0 +1,29 @@
+#include "stdafx.h"
+
+#include "Spin.h"
+
+
+Spin& Spin::GetInstance()
+{
+	static Spin Instance;
+
+	return &Instance;
+}
+
+
+VOID Spin::Enter( Miner* pMiner )
+{
+
+}
+
+
+VOID Spin::Execute( Miner* pMiner )
+{
+
+}
+
+
+VOID Spin::Exit( Miner* pMiner )
+{
+
+}
