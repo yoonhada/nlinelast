dffe9b40550823725f228119bffa40349787b742
chunju2@naver.com <chunju2@naver.com@d02aaf57-2019-c8cd-d06c-d029ef2af4e0>
2011-11-01 05:17:19 +0000

    AI 모듈 추가

---
+++ trunk/LastProject/Source/Battle.cpp
@@ -0,0 +1,29 @@
+#include "stdafx.h"
+
+#include "Battle.h"
+
+
+Battle& Battle::GetInstance()
+{
+	static Battle Instance;
+
+	return &Instance;
+}
+
+
+VOID Battle::Enter( Miner* pMiner )
+{
+
+}
+
+
+VOID Battle::Execute( Miner* pMiner )
+{
+
+}
+
+
+VOID Battle::Exit( Miner* pMiner )
+{
+
+}
---
+++ trunk/LastProject/Source/Dash.h
@@ -0,0 +1,25 @@
+#ifndef _DASH_H_
+#define _DASH_H_
+
+
+class Dash
+{
+private:
+	Dash(){}
+
+	Dash( const Dash& rhs );
+	Dash& operator=( const Dash& rhs );
+
+
+public:
+	virtual ~Dash(){}
+
+	static Dash& GetInstance();
+
+	virtual VOID Enter( Miner* pMiner );
+	virtual VOID Execute( Miner* pMiner );
+	virtual VOID Exit( Miner* pMiner );
+};
+
+
+#endif
